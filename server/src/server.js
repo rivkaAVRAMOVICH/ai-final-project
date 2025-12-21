24dfc38ae6c01c575efdb3c3f6ca822cf712af3c
rivkaAVRAMOVICH <mavramovich7@gmail.com>
2025-12-21 03:06:53 +0200

    Resolve all merge conflicts

--- server/src/server.js
+++ server/src/server.js
@@ -25,6 +25,6 @@ export default app;
 // app.listen(PORT, () => {
 //   console.log(`Server running at http://localhost:${PORT}`);
 // });
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server running at http://0.0.0.0:${PORT}`);
-// });
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
+});
